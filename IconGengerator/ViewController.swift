ee3c84ea3b4f9db5d1d9681e371a0c8d46e3392b
limao <morning@thecover.co>
2020-12-08 14:44:12 +0800

    Initial Commit

---
+++ IconGengerator/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  IconGengerator
+//
+//  Created by mao li on 2020/12/8.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
